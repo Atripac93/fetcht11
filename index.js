ea14a48acd5fe3f530cc9682e0541fb147dc5d81
Carlo Atripaldi <carloatrica@gmail.com>
2024-01-18 17:02:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
-const si = document.querySelector(".oi");
-const testo = document.createTextNode("ueueeu");
-
-si.appendChild(testo);
-document.getElementById("contenitore").appendChild(si);
-
-const tic = function () {
-  const input = document.querySelector("input");
-  console.log(input.value);
+const mostraTarget = function () {
+  console.log(mostraTarget.value);
 };
-const sisi = function () {
-  const input = document.querySelector("input");
-  console.log(input.value);
-  input = "";
+
+const button = document.querySelector(".cicc");
+button.addEventListener("change", mostraTarget);
+
+const dammiValore = function () {
+  const valor = document.querySelector(".valu");
+  return valor.value;
 };
+const addTask = function () {
+  const list = document.querySelector(".addTask");
+  list.innerText += `<li> ${dammiValore()} </li>`;
+};
+const input = document.querySelector(".valu");
+input.addEventListener("change", addTask);
+//
+const carlo = () => {
+  let fabio = document.querySelector(".marco");
+  console.log(fabio.value);
+  fabio.value = "";
+};
+const concetta = document.querySelector(".fulvio");
+concetta.addEventListener("click", carlo);
+
+const mirko = () => {
+  const salvatore = document.querySelector(".marco");
+  return salvatore.value;
+};
+
+const luigi = () => {
+  const micio = document.querySelector(".andrea");
+  micio.innerHTML += `<li>${mirko()}</li>`;
+};
+const sabrina = document.querySelector(".marco");
+sabrina.addEventListener("change", luigi);
+
+
+
